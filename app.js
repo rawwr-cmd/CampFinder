a35a51cb6941a2a6e9d319f47e6845ff7be396c5
rawwr <akshayace0302@gmail.com>
2022-04-22 22:39:56 +0530

    joi validation middleware

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const { campgroundSchema } = require("./schemas");
+const catchAsync = require("./utils/catchAsync");
+const expressError = require("./utils/ExpressError");
 const ejsMate = require("ejs-mate");
 const Campground = require("./models/campground");
 
@@ -22,6 +25,18 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+//Joi Validation Middleware
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  // console.log(result);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new expressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -35,45 +50,79 @@ app.get("/makecampground", async (req, res) => {
   res.send(camp);
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("camp/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({});
+    res.render("camp/index", { campgrounds });
+  })
+);
 
 app.get("/campgrounds/new", async (req, res) => {
   res.render("camp/new");
 });
 
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
+app.post(
+  "/campgrounds",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    //   throw new expressError("Invalid Campground Data", 404);
+
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    // console.log(campground);
+    res.render("camp/show", { campground });
+  })
+);
+
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("camp/edit", { campground });
+  })
+);
+
+app.put(
+  "/campgrounds/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    // res.send("it worked");
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground.id}`);
+  })
+);
+
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new expressError("Page not Found", 400));
 });
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  // console.log(campground);
-  res.render("camp/show", { campground });
-});
-
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("camp/edit", { campground });
-});
-
-app.put("/campgrounds/:id", async (req, res) => {
-  // res.send("it worked");
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  res.redirect(`/campgrounds/${campground.id}`);
-});
-
-app.delete("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  // console.log(err.message);
+  if (!err.message) err.message = "Oh No, Something went Wrong";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
