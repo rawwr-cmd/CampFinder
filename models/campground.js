38702ff6a3e487c0982a333512439ff087f5a735
rawwr <akshayace0302@gmail.com>
2022-04-26 20:10:32 +0530

    campground Delete Middleware/findOneAndDelete

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -8,6 +8,20 @@ const CampgroundSchema = new Schema({
   price: Number,
   description: String,
   location: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+});
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  // console.log(doc);
+  if (doc) {
+    const res = await Review.deleteMany({ _id: { $in: doc.reviews } });
+    // console.log(res);
+  }
 });
 
 const Campground = mongoose.model("Campground", CampgroundSchema);
