3aabe572f3aa529c46cae22efe44d32511fd5461
rawwr <akshayace0302@gmail.com>
2022-04-29 22:02:39 +0530

    adding returnTo behaviour for logins

--- middleware.js
+++ middleware.js
@@ -1,6 +1,11 @@
 //coming from passport
 module.exports.isLoggedIn = (req, res, next) => {
+  //   console.log("req.user...", req.user);
+
   if (!req.isAuthenticated()) {
+    // console.log(req.path, req.originalUrl);
+    //returning to the page from where we are logging in
+    req.session.returnTo = req.originalUrl;
     req.flash("error", "you must be signed in first!");
     return res.redirect("/login");
   }
