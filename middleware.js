8131317d6e92e159667116da82bae50240dae396
rawwr <akshayace0302@gmail.com>
2022-04-29 18:11:03 +0530

    isLoggedIn middleware

---
+++ middleware.js
@@ -0,0 +1,8 @@
+//coming from passport
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.flash("error", "you must be signed in first!");
+    return res.redirect("/login");
+  }
+  next();
+};
