5487a0bb649558a7f318344363121d9cccf46678
rawwr <akshayace0302@gmail.com>
2022-05-01 22:04:56 +0530

    adding an author to the campground

--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "626c13ee4af39820b8af9e11",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
