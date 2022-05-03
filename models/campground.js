5daca2d95a777a5df2f431a752ef3ac196ba4619
rawwr <akshayace0302@gmail.com>
2022-05-03 11:54:55 +0530

    adding geoJson

--- models/campground.js
+++ models/campground.js
@@ -18,7 +18,17 @@ const CampgroundSchema = new Schema({
   images: [ImageSchema],
   price: Number,
   description: String,
-  location: String,
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   author: {
     type: Schema.Types.ObjectId,
     ref: "User",
