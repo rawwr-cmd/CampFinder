7ac3fbb196567b5de0484b0aedd9cb7caf1c188d
rawwr <akshayace0302@gmail.com>
2022-05-04 21:44:25 +0530

    ready to try deploying

--- app.js
+++ app.js
@@ -19,10 +19,14 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
+const MongoDBStore = require("connect-mongo");
+
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+// "mongodb://localhost:27017/yelp-camp"
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -39,10 +43,23 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(mongoSanitize());
+app.use(mongoSanitize({ replaceWith: "_" }));
+
+const secret = process.env.SECRET || "thisisabettersession";
+
+const store = MongoDBStore.create({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
 
 const sessionConfig = {
-  secret: "thisisabettersession",
+  store,
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
